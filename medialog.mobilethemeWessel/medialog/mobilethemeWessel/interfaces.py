bff20397a4f6da4ebc3b0b0351b1816baf47a7ea
Espen Moe-Nilssen <espen@medialog.no>
2014-09-23 17:32:50 +0200

    addded all

---
+++ medialog.mobilethemeWessel/medialog/mobilethemeWessel/interfaces.py
@@ -0,0 +1,11 @@
+from zope.interface import Interface
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('medialog.mobilethemeWessel')
+
+
+class IMobilethemeWesselLayer(Interface):
+    """A layer specific to medialog.mobilethemeWessel
+        """
+ 
