96a17d501d4b9b214ca34c36ff7fb48a35cacada
Espen Moe-Nilssen <espen@medialog.no>
2014-09-24 12:02:27 +0200

    Added loge haven

---
+++ medialog.mobilethemeLogehaven/medialog/mobilethemeLogehaven/interfaces.py
@@ -0,0 +1,11 @@
+from zope.interface import Interface
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('medialog.mobilethemeLogehaven')
+
+
+class IMobilethemeLogehavenLayer(Interface):
+    """A layer specific to medialog.mobilethemeLogehaven
+        """
+ 
