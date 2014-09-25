bce6218160c2f5c9c2a6d5b8a697f3450be3d9ee
Espen Moe-Nilssen <espen@medialog.no>
2014-09-25 09:53:35 +0200

    added wesselbar

---
+++ medialog.mobilethemeWesselbar/medialog/mobilethemeWesselbar/interfaces.py
@@ -0,0 +1,11 @@
+from zope.interface import Interface
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('medialog.mobilethemeWesselbar')
+
+
+class IMobilethemeWesselbarLayer(Interface):
+    """A layer specific to medialog.mobilethemeWesselbar
+        """
+ 
